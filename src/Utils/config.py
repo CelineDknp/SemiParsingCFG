92bf96d64e9d861fd6c73cbd2b7ede97a8337496
CelineDknp <celine.deknop@uclouvain.be>
2022-04-20 17:02:34 +0200

    Pretty config file

--- src/Utils/config.py
+++ src/Utils/config.py
@@ -1,31 +1,48 @@
-import regex as re
+import re
 from .constants import *
 
+"""Condition structures
+start: regex for the start of the condition structure
+single_branch | multiple_branch : regex for a conditon branch
+end: regex for the end of the condition
+*condition_delimiter: regex for a condition delimiter, if any
+*mandatory_delimiter: boolean signifying if the condition delimiter is mandatory
+"""
+conditions = [
+    {"start":r"\sIF(\s)+", "condition_delimiter":r"\sTHEN(\s)+", "mandatory_delimiter":False, "single_branch":r"ELSE\s", "end":r"\sEND-IF"}, 
+    {"start":r"\sEVALUATE(\s)+", "multiple_branch":r"\sWHEN", "end":r"\sEND-EVALUATE"}
+]
+
+"""Loops structures
+start: regex for the start of the loop structure
+type: label | multiple_label | control-flow
+goback: True if the execution goes back after the paragraph, false if not
+for label-type: 
+    inline-label: regex for the label after the loop structure
+    label-regex: regex for the label in the code body
+for control-flow:
+    control-type: node type towards wich the loop jumps (see node types in Utils/contants.py)
+    control-regex: regex for the element towards which  the loop jumps
+"""
+loops = [
+    {"start":r"\sGO(\s)?TO(\s)+", "type":"label", "goback":False, "inline-label":r"([^\s*.])+(\s)*", "label-regex":r"^\s{7}([^\s*])+(\s)*\."},
+    {"start":r"\sPERFORM(\s)+", "type":"multiple_label", "goback":True, "separator":r"(\s)+THRU(\s)+", "inline-label":r"([^\s*.])+(\s)*", "label-regex":r"^\s{7}([^\s*])+(\s)*\."},
+    {"start":r"\sPERFORM(\s)+", "type":"label", "goback":True, "inline-label":r"([^\s*.])+(\s)*", "label-regex":r"^\s{7}([^\s*])+(\s)*\."},
+    {"start":r"\sNEXT\sSENTENCE(\s)*", "type":"control-flow", "goback":False, "control-type": NODE_COND_END_ANY, "control-regex":r"\.(\s)*$"}
+]
+"""Structure that we want to parse
+start: regex for the start of the structure
+end: regex for the end of the structure
+[Note] The text found in between the start and end regex will be parsed
+"""
+parsable = [{"start":r"EXEC\sSQL", "end":r"END-EXEC"}]
+
 #All structures that can contain code but should be ignored (typically, strings and comments)
-IGNORE = "ignore"
-to_ignore = [{"regex": r"(('(.)*$\s{7}-\s*))?('(.)*')", "type":"string"}, {"regex":r"((\s)*\*(.)*$)+", "type":"comment"}]
-
-#Condition structures, in the form of list of dicts {start, n_branch|single_branch, end}
-CONDITION = "condition"
-conditions = [{"start":r"\sIF(\s)+", "condition_delimiter":r"\sTHEN(\s)+", "mandatory_delimiter":False, "single_branch":"ELSE\s", "end":"\sEND-IF"}, {"start":r"\sEVALUATE(\s)+", "multiple_branch":"\sWHEN", "end":"\sEND-EVALUATE"}]
-
-#Loops structures
-# start: regex for the start of the loop structure
-# type: label, multiple_label or control-flow
-# goback: True if the execution goes back after the paragraph, false if not
-# for label-type: inline-label regex and label-regex regex (first one for the label in the loop call, second for the label in the code body)
-# for control-type, control-type = regex towards control-flow
-LOOP = "loop"
-loops = [{"start":r"\sGO(\s)?TO(\s)+", "type":"label", "goback":False, "inline-label":r"([^\s*.])+(\s)*", "label-regex":r"^\s{7}([^\s*])+(\s)*\."},
-         {"start":r"\sPERFORM(\s)+", "type":"multiple_label", "goback":True, "separator":r"(\s)+THRU(\s)*", "inline-label":r"([^\s*.])+(\s)*", "label-regex":r"^\s{7}([^\s*])+(\s)*\."},
-         {"start":r"\sPERFORM(\s)+", "type":"label", "goback":True, "inline-label":r"([^\s*.])+(\s)*", "label-regex":r"^\s{7}([^\s*])+(\s)*\."},
-         {"start":r"\sNEXT\sSENTENCE(\s)*", "type":"control-flow", "goback":False, "control-type": NODE_COND_END_ANY, "control-regex":r"\.(\s)*$"}
-         ]
-
-PARSABLE = "parsable"
-parsable = [{"start":r"EXEC\sSQL", "end":"END-EXEC"}]
+to_ignore = [
+    {"regex": r"(('(.)*$\s{7}-\s*))?('(.)*')", "type":"string"}, 
+    {"regex":r"((\s)*\*(.)*$)+", "type":"comment"}
+]
 
 #Language special case
-SPECIAL = "special"
 special = [{"regex":r"(\s)*PROCEDURE\sDIVISION", "effect":"start_parse"}, {"regex":r"\.(\s)*$", "effect":"close_all"}]
 
