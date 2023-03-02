54e890255557694844f1b861c77933c088a19f8d
CelineDknp <celine.deknop@uclouvain.be>
2023-03-02 11:41:30 +0100

    removed rec depth when multiple labels loop node

--- src/Nodes/BlockLoopNode.py
+++ src/Nodes/BlockLoopNode.py
@@ -1,4 +1,5 @@
 from .LoopNode import LoopNode
+from .LabelNode import LabelNode
 from Anchors import LoopAnchor
 from Utils.config import *
 from Utils.utils import *
@@ -41,7 +42,7 @@ class BlockLoopNode(LoopNode):
 		return self.target_node
 
 	def get_childs_h(self):
-		if len(self.get_childs()) == 1:
+		if len(self.get_childs()) == 1 and not isinstance(self.childs[0], LabelNode):
 			return self.get_childs()
 		res = []
 		for c in self.get_childs():
